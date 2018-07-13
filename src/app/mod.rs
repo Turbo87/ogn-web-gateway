27cf33d7840b880ade3577005829bc7930d241ab
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-07-13 22:06:40 +0200

    app: Simplify websocket route attachment

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -23,8 +23,7 @@ pub fn build_app(db: Addr<DbExecutor>, gateway: Addr<Gateway>) -> App<AppState> 
         }))
         .resource("/api/status", |r| r.method(http::Method::GET).with(status::status))
         .route("/api/{id}/positions", http::Method::GET, positions::positions)
-        // websocket
-        .resource("/ws/", |r| r.route().f(|req| ws::start(req, WSClient::default())))
+        .route("/ws/", http::Method::GET, |req| ws::start(&req, WSClient::default()))
         // static resources
         .handler("/static/", fs::StaticFiles::new("static/").unwrap())
 }
