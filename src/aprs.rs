fc510e22fe81f85f04aa56befce60a2b7581b7e6
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-07-13 21:10:43 +0200

    Add support for "position precision enhancement" in APRS messages

--- src/aprs.rs
+++ src/aprs.rs
@@ -19,7 +19,7 @@ pub fn parse<'a>(line: &'a str) -> Option<APRSPosition<'a>> {
     // FLRDD87AC>APRS,qAS,LFQB:/141950h4818.33N/00401.87E'014/034/A=005199 !W26! id06DD87AC +218fpm +2.5rot 17.8dB 0e -2.4kHz gps3x4 -1.0dBm
 
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"(?x)
+        static ref RE: Regex = Regex::new(r#"(?x)
             [^:]:                      # header incl. separator
             /                          # position report indicator
             (?P<time>\d{6})h           # time in HHMMSS incl. `h` indicator
@@ -34,9 +34,10 @@ pub fn parse<'a>(line: &'a str) -> Option<APRSPosition<'a>> {
             (?P<speed>\d{3})           # speed in knots
             /                          # separator
             A=(?P<alt>\d{6})           # altitude in feet (converted to meters)
+            (?:\x20!W(?P<ppe>\d\d)!)?  # position precision enhancement
             .*                         # (irrelevant)
             \x20id(?P<id>[\dA-F]{8})   # OGN device ID
-        ").unwrap();
+        "#).unwrap();
     }
 
     RE.captures(line).map(|caps| {
@@ -49,7 +50,12 @@ pub fn parse<'a>(line: &'a str) -> Option<APRSPosition<'a>> {
 
         let latitude = {
             let raw_angle = caps.name("lat").unwrap().as_str();
-            let angle = raw_angle[0..2].parse::<f64>().unwrap() + raw_angle[2..].parse::<f64>().unwrap() / 60.;
+            let mut angle = raw_angle[0..2].parse::<f64>().unwrap() + raw_angle[2..].parse::<f64>().unwrap() / 60.;
+
+            if let Some(ppe) = caps.name("ppe") {
+                let enhancement = ppe.as_str()[0..1].parse::<f64>().unwrap();
+                angle += enhancement / 60000.
+            }
 
             let hemisphere = caps.name("lat_sign").unwrap().as_str();
             if hemisphere == "N" { angle } else { -angle }
@@ -57,7 +63,12 @@ pub fn parse<'a>(line: &'a str) -> Option<APRSPosition<'a>> {
 
         let longitude = {
             let raw_angle = caps.name("lon").unwrap().as_str();
-            let angle = raw_angle[0..3].parse::<f64>().unwrap() + raw_angle[3..].parse::<f64>().unwrap() / 60.;
+            let mut angle = raw_angle[0..3].parse::<f64>().unwrap() + raw_angle[3..].parse::<f64>().unwrap() / 60.;
+
+            if let Some(ppe) = caps.name("ppe") {
+                let enhancement = ppe.as_str()[1..2].parse::<f64>().unwrap();
+                angle += enhancement / 60000.
+            }
 
             let hemisphere = caps.name("lon_sign").unwrap().as_str();
             if hemisphere == "E" { angle } else { -angle }
@@ -83,8 +94,8 @@ mod tests {
         let position = result.unwrap();
         assert_eq!(position.id, "06DD9612");
         assert_eq!(position.time, "14:19:56".parse().unwrap());
-        assert_relative_eq!(position.latitude, 49. + 11.18 / 60.);
-        assert_relative_eq!(position.longitude, 8. + 15.93 / 60.);
+        assert_relative_eq!(position.latitude, 49. + 11.187 / 60.);
+        assert_relative_eq!(position.longitude, 8. + 15.935 / 60.);
         assert_relative_eq!(position.altitude, 1132.6368);
         assert_eq!(position.course, 126);
     }
@@ -97,15 +108,15 @@ mod tests {
         let position = result.unwrap();
         assert_eq!(position.id, "214060D7");
         assert_eq!(position.time, "14:19:53".parse().unwrap());
-        assert_relative_eq!(position.latitude, 51. + 47.03 / 60.);
-        assert_relative_eq!(position.longitude, -(1. + 9.00 / 60.));
+        assert_relative_eq!(position.latitude, 51. + 47.035 / 60.);
+        assert_relative_eq!(position.longitude, -(1. + 9.000 / 60.));
         assert_relative_eq!(position.altitude, 1037.844);
         assert_eq!(position.course, 210);
     }
 
     #[test]
     fn test_parse_3() {
-        let result = parse(r"FLRDD87AC>APRS,qAS,LFQB:/141950h4818.33N/00401.87E'014/034/A=005199 !W26! id06DD87AC +218fpm +2.5rot 17.8dB 0e -2.4kHz gps3x4 -1.0dBm");
+        let result = parse(r"FLRDD87AC>APRS,qAS,LFQB:/141950h4818.33N/00401.87E'014/034/A=005199 id06DD87AC +218fpm +2.5rot 17.8dB 0e -2.4kHz gps3x4 -1.0dBm");
         assert!(result.is_some());
 
         let position = result.unwrap();
