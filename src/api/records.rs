f1a21aea5dc7323252954f03c484216eacbe0410
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-07-13 23:10:33 +0200

    api/records: Simplify implementation by using extractors

--- src/api/records.rs
+++ src/api/records.rs
@@ -1,13 +1,12 @@
-use actix_web::{HttpRequest, Responder, Json, Error, AsyncResponder};
+use actix_web::*;
 use futures::Future;
 
 use ::app::AppState;
 use ::db;
 
-pub fn get(req: HttpRequest<AppState>) -> impl Responder {
-    let ogn_id = req.match_info().get("id").unwrap().to_owned();
 
-    req.state().db.send(db::ReadOGNPositions { ogn_id }).from_err::<Error>()
+pub fn get((id, state): (Path<String>, State<AppState>)) -> impl Responder {
+    state.db.send(db::ReadOGNPositions { ogn_id: id.to_owned() }).from_err::<Error>()
         .and_then(|res: Option<Vec<db::models::OGNPosition>>| {
             let ogn_id_positions: Vec<String> = res.unwrap_or_else(|| Vec::new()).iter()
                 .map(|pos| format!(
