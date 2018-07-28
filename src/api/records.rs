9588d92fab27b30acb08d8ba567694b9e3e4afe5
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-07-28 22:19:34 +0200

    api/records: Support requests for multiple IDs

--- src/api/records.rs
+++ src/api/records.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use actix_web::*;
 use chrono::prelude::*;
 use futures::Future;
@@ -20,21 +22,26 @@ pub fn get((id, query, state): (Path<String>, Query<GetQueryParams>, State<AppSt
         .and_then(|it| NaiveDateTime::from_timestamp_opt(it, 0))
         .map(|it| DateTime::from_utc(it, Utc));
 
-    let db_request = db::ReadOGNPositions { ids: vec![id.to_owned()], after, before };
+    let ids: Vec<_> = id.split(",").map(|s| s.to_owned()).collect();
+    let mut id_records_map: HashMap<String, Vec<String>> = ids.iter().map(|id| (id.to_owned(), Vec::new())).collect();
+
+    let db_request = db::ReadOGNPositions { ids: ids.clone(), after, before };
 
     state.db.send(db_request).from_err::<Error>()
         .and_then(|res: Option<Vec<db::models::OGNPosition>>| {
-            let ogn_id_positions: Vec<String> = res.unwrap_or_else(|| Vec::new()).iter()
-                .map(|pos| format!(
+            res.unwrap_or_else(|| Vec::new()).iter().for_each(|pos| {
+                let list = id_records_map.get_mut(&pos.ogn_id).unwrap();
+
+                list.push(format!(
                     "{}|{:.6}|{:.6}|{}",
                     pos.time.timestamp(),
                     pos.longitude,
                     pos.latitude,
                     pos.altitude,
-                ))
-                .collect();
+                ));
+            });
 
-            Ok(Json(ogn_id_positions))
+            Ok(Json(id_records_map))
         })
         .responder()
 }
