2b4acc1115ded25f40fe0be9d35fcb477e297a3b
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-01-28 16:39:08 +0100

    app: Use `Cors` middleware for the API

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 use actix::*;
+use actix_web::middleware::cors::Cors;
 use actix_web::middleware::Logger;
 use actix_web::*;
 
@@ -14,14 +15,18 @@ pub struct AppState {
 pub fn build_app(redis: Addr<RedisExecutor>, gateway: Addr<Gateway>) -> App<AppState> {
     App::with_state(AppState { redis, gateway })
         .middleware(Logger::default())
+        .configure(|app| {
+            Cors::for_app(app)
+                .resource("/api/cors-proxy/{uri:.+}", |r| {
+                    r.get().with(api::cors_proxy::get)
+                })
+                .resource("/api/ddb", |r| r.get().with(api::ddb::get))
+                .resource("/api/status", |r| r.get().with(api::status::get))
+                .resource("/api/records/{id}", |r| r.get().with(api::records::get))
+                .resource("/api/live", |r| r.get().with(api::live::get))
+                .register()
+        })
         .route("/", http::Method::GET, |_: HttpRequest<_>| {
             fs::NamedFile::open("static/websocket.html")
         })
-        .resource("/api/cors-proxy/{uri:.+}", |r| {
-            r.get().with(api::cors_proxy::get)
-        })
-        .resource("/api/ddb", |r| r.get().with(api::ddb::get))
-        .resource("/api/status", |r| r.get().with(api::status::get))
-        .resource("/api/records/{id}", |r| r.get().with(api::records::get))
-        .resource("/api/live", |r| r.get().with(api::live::get))
 }
